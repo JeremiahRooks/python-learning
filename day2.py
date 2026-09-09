c9cb7e0e4a9c3312c80be03e206b1a6061bbdb55
Jeremiah Rooks Boyd Featherson <j.rooksboydfeatherson@gmail.com>
2026-09-08 20:20:37 -0400

    Completed Day 2 Python

---
+++ day2.py
@@ -0,0 +1,10 @@
+name = "Jeremiah"
+age = 16
+favorite_game = "Roblox"
+favorite_food = "Burgers"
+current_goal  = "Understand the fundamentals of coding and create scripts and programs for games."
+print(name)
+print(age)
+print(favorite_game)
+print(favorite_food)
+print(current_goal)
