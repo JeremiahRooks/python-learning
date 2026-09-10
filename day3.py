94937a2fc6ecb4b825cba5afddbb635fd88aa2ea
Jeremiah Rooks Boyd Featherson <j.rooksboydfeatherson@gmail.com>
2026-09-09 20:50:12 -0400

    Completed Day 3 Python

---
+++ day3.py
@@ -0,0 +1,7 @@
+first_name = "Jeremiah"
+last_name = "Rooks"
+favorite_game = "My favorite game is Roblox"
+favorite_food = "My favorite food is Burgers"
+goal = "My goal is to understand the fundamentals of coding and create scripts and programs for games."
+introduction = " My name is " + first_name + " " + last_name + " " + favorite_game + " " + favorite_food + " " + goal
+print(introduction)
