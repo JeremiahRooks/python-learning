4d4dde86e5551215ba756846139b82740945cc3a
Jeremiah Rooks Boyd Featherson <j.rooksboydfeatherson@gmail.com>
2026-09-10 21:03:47 -0400

    Completed Day 4 Python

---
+++ day4.py
@@ -0,0 +1,7 @@
+hours_worked = 13.60
+hourly_pay = 14.00
+gross_pay = hours_worked * hourly_pay
+print("Gross pay: $", gross_pay)
+car_savings_percent = 0.10
+car_savings = gross_pay * car_savings_percent
+print("Car savings: $", car_savings)
